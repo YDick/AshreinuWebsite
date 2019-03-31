f282fdb075a1843130037389c82101f6b1113ca5
YDick <ydick1998@gmail.com>
2019-03-30 23:23:02 -0400

    added new page, trying to make encrypted password for new fundraiser

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -2,9 +2,27 @@ class Fundraiser < ApplicationRecord
 
     has_secure_password
 
+    attr_accessor :password
 
-    def full_name
-        "#{fname} #{lname}"
-    end
+    validates :password, :confirmation => true #password_confirmation attr
+    validates_length_of :password, :in => 2..20, :on => :create
+
+    # before_save :encrypt_password
+    # after_save :clear_password
+
+    # def encrypt_password
+    #     if password.present?
+    #         self.salt = BCrypt::Engine.generate_salt
+    #         self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+    #     end
+    # end
+    #     def clear_password
+    #     self.password = nil
+    # end
+
+
+    # def full_name
+    #     "#{fname} #{lname}"
+    # end
     
 end

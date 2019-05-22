17722d3bf34cd242c2c9f9cfc0a45f6467e297df
YDick <ydick1998@gmail.com>
2019-05-21 22:33:26 -0400

    conntorller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,6 @@
 class MapController < ApplicationController
   def view
+    @maps_api_key = Figaro.env.google_maps_api_key
     @bjhs = Place.find_by(name:"BJHS")
     @overnight = Place.find_by(name:"Overnight")
   end

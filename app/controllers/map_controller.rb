ae739e394407b1f9637cc8bcc85ff6ee07103c8b
YDick <ydick1998@gmail.com>
2019-05-21 22:36:20 -0400

    api problems

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,7 @@
 class MapController < ApplicationController
   def view
     @maps_api_key = Figaro.env.google_maps_api_key
+    puts ".....................................#{@maps_api_key} #{@byjs}"
     @bjhs = Place.find_by(name:"BJHS")
     @overnight = Place.find_by(name:"Overnight")
   end

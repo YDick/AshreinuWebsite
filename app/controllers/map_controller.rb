a389ed61dded3a5d5de66facfb696985182d169d
YDick <ydick1998@gmail.com>
2019-05-30 18:10:40 -0400

    background image for maps

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,10 +1,8 @@
 class MapController < ApplicationController
+
   def view
     @maps_api_key = Figaro.env.google_maps_api_key
-    puts ".....................................#{@maps_api_key}"
     @bjhs = Place.find_by(name:"BJHS")
-    puts ".....................................#{@bjhs}"
     @overnight = Place.find_by(name:"Overnight")
-    puts ".....................................#{@overnight}"
   end
 end

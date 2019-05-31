61c22ee2608230913cd499a1e78a8c1875d02d82
YDick <ydick1998@gmail.com>
2019-05-31 12:34:18 -0400

    auto reply from contact us

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,34 +9,17 @@ class MessagesController < ApplicationController
 
         respond_to do |format|
             if @message.valid?
-              MailerMailer.with(message: params[:message]).contact.deliver  
+              MailerMailer.with(message: params[:message]).contact.deliver
               format.html {redirect_to(root_path) }
+              MailerMailer.with(message: params[:message]).auto_reply.deliver    
+              format.html 
             else
-                flash.now.alert = "Please fill in all the required boxes"
+              flash.now.alert = "Please fill in all the required boxes"
               format.html { render action: 'new'  }
             end
           end
 
-        # @message.request = request
-        # respond_to do |format|
-        #   if @message.deliver
-        #     # re-initialize Home object for cleared form
-        #     @contact = Home.new
-        #     format.html { render 'index'}
-        #     format.js   { flash.now[:success] =  "Thank you for your message. I'll get back to you soon!" }
-        #   else
-        #     format.html { render 'index' }
-        #     format.js   { flash.now[:error] = "Message did not send." }
-        #   end
 
-            # if @message.valid?
-            #     MailerMailer.contact(@message).deliver_now
-            #     redirect_to new_message_url
-            #     flash[:notice] = "We have received your message and will be in touch soon!"
-            # else
-            #     flash[:notice] = "There was an error sending your message. Please try again."
-            #     render :new
-            # end
 
     end
 
--- app/mailers/mailer_mailer.rb
+++ app/mailers/mailer_mailer.rb
@@ -1,13 +1,20 @@
 class MailerMailer < ApplicationMailer
     default from: 'justatestomg@gmail.com'
-    default to: "joodeek@gmail.com"
 
     def contact
       @name = params[:message][:name]
       @body = params[:message][:body]
       @email = params[:message][:email]
       @phone = params[:message][:phone_number]
-      mail(subject: 'Someone has contacted us!')
+      mail(to: "joodeek@gmail.com", subject: 'Someone has contacted us!')
+    end
+
+    def auto_reply
+      @name = params[:message][:name]
+      @body = params[:message][:body]
+      @email = params[:message][:email]
+      @phone = params[:message][:phone_number]
+      mail(to: @email, subject: "We Have Received Your Message!")
     end
 
     

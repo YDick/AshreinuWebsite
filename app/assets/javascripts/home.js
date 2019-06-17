990b69c2bb4fe34d7369d77de18392fc5de7fc20
YDick <ydick1998@gmail.com>
2019-06-16 20:28:46 -0400

    clear navbar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $('.carousel').carousel({interval: 7000});
-  });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-50px";
+  }

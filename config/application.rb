628978c3fbfdab0b1d77b85e5df4ff589d5d2700
YDick <ydick1998@gmail.com>
2019-06-20 15:17:11 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Ashreinu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # config.assets.initialize_on_precompile = false
 
     # fonts
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")

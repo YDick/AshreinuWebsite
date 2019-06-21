9777d919e09a40d739b712f578774d3e7362e387
YDick <ydick1998@gmail.com>
2019-06-21 15:53:50 -0400

    rm navbar scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,9 @@
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector(".navbar").style.top = "0";
-  } else {
-    document.querySelector(".navbar").style.top = "-50px";
-  }
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.querySelector(".navbar").style.top = "0";
+//   } else {
+//     document.querySelector(".navbar").style.top = "-50px";
+//   }
+// }

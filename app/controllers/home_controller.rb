ea2e9a137dee52247d8854bfcbd15d285e7ca18c
YDick <ydick1998@gmail.com>
2019-06-21 16:36:31 -0400

    rename pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
     def about
     end
+
+    def download_nightapp
+        # send_file(
+            # "#{Rails.root}/public/applications/Ashreinu_2019_overnight.pdf",
+        #     filename: "Ashreinu Brochure 2019_overnight_in house.pdf",
+        #     type: "application/pdf"
+        #   )
+        @overnight_app = Rails.root/public/applications/Ashreinu_2019_overnight.pdf
+        redirect_to @overnight_app.attachment.path
+    end
 end
